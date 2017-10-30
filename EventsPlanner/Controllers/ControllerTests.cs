63a6fec3a82d26f1bf70eca880e31359c3405e71
User <User@WIN7_PC>
2017-10-30 21:51:32 +0300

    ok

---
+++ EventsPlanner/Controllers/ControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EventsPlanner.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+
+namespace EventsPlanner.Controllers
+{
+    [TestClass]
+    public class ControllerTests
+    {
+        [TestMethod]
+        public void MyEventsTest()
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            var u = db.Users.FirstOrDefault(f => f.UserName == "7775494@mail.ru");
+            Assert.AreEqual(u.UserName, "7775494@mail.ru");
+
+        }
+    }
+}
